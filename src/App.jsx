05d5589020b4cb360e0181889d035abdd8bf5a28
Ravindra Kamble <write2ravi14@gmail.com>
2025-11-16 14:12:29 +0530

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Life from "./pages/Life";
 
 function App() {
-
-
   return (
     <>
-     <Routes>
-<Route path="/life" element={<Life />} />
-     </Routes>
-      
+      <Routes>
+        <Route path="/" element={<Life />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
